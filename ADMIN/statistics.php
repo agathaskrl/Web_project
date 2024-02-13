d1104b48cf0229e6e4cbbf8064fbd991a114cbcc
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-02-13 13:31:19 +0200

    Add files via upload

---
+++ ADMIN/statistics.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lan="en" and dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Statistics</title>
+        <link rel="stylesheet" href="adstyle.css?v=4">
+    </head>     
+    <div class="main">
+            <div class="navbar">
+            <ul> 
+                    <li><a href="adindex.php">HOME</a></li>  
+                        <li><a href="warehouse.php">WAREHOUSE</a></li>
+                        <li><a href="statistics.php">STATISTICS</a></li>
+                        <li><a href="savsignup.php">CREATE ACCOUNT</a></li>
+                        <li><a href="announcement.php">ANNOUNCEMENT</a></li>
+                        <li><a href="logout.php">LOG OUT</a></li>              
+                    </ul>
+                </div>
+<body>
+<?php
+// Function to check if the user is logged in
+function checkLoggedIn() {
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        echo '<div style="text-align: center; padding: 80px; background-color: rgb(247, 240, 235); color: rgba(76, 56, 30, 1); ">';
+        echo 'User not logged in. Please <a href="login.php">Log in!</a>.';
+        echo '</div>';
+        exit(); // Exit the script
+    }
+}
+checkLoggedIn(); // Call the function to check if the user is logged in
+?>
+
+<br>
