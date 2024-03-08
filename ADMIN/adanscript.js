e062855c1dfa5f8a704e6779205e27905f38ed98
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-03-08 17:01:43 +0200

    Update adanscript.js
    Added function for expanding the announcement form and getting the autocomplete

--- ADMIN/adanscript.js
+++ ADMIN/adanscript.js
@@ -1,32 +1,71 @@
-$(function() {
-    $("#autocomplete").autocomplete({
-        source: function(request, response) {
-            console.log('Fetching data for:', request.term);
+$(function () {
+  //function for autocomplete
+  function initializeAutocomplete(element) {
+    element.autocomplete({
+      source: function (request, response) {
+        console.log("Fetching data for:", request.term);
 
-            // Fetch data
-            $.ajax({
-                url: "ajax.php",
-                type: 'post',
-                dataType: "json",
-                data: {
-                    search: request.term
-                },
-                success: function(data) {
-                    response(data);
-                    console.log('Data received:', data);
-                }
-            });
-        },
-        select: function(event, ui) {
-            console.log('Item selected:', ui.item.label);
-            // Set selection
-            $('#autocomplete').val(ui.item.label); // display the selected text
-            return false;
-        },
-        focus: function(event, ui) {
-            console.log('Item focused:', ui.item.label);
-            $("#autocomplete").val(ui.item.label);
-            return false;
-        }
+        // Fetch data
+        $.ajax({
+          url: "ajax.php",
+          type: "post",
+          dataType: "json",
+          data: {
+            search: request.term,
+          },
+          success: function (data) {
+            response(data);
+            console.log("Data received:", data);
+          },
+        });
+      },
+      select: function (event, ui) {
+        console.log("Item selected:", ui.item.label);
+        // Set selection
+        element.val(ui.item.label); // display the selected text
+        return false;
+      },
+      focus: function (event, ui) {
+        console.log("Item focused:", ui.item.label);
+        element.val(ui.item.label);
+        return false;
+      },
     });
+  }
+
+  initializeAutocomplete($("#autocomplete"));
+
+  $(document).ready(function () {
+    let counter = 1;
+
+    $("#addann").on("click", function () {
+      counter++;
+
+      //Expand the form
+      var newInputFields =
+        '<div class="input-box">' +
+        '<label for="autocomplete"> Need for item:</label>' +
+        `<input type="text" name="product-input-${counter}"` +
+        counter +
+        '" placeholder="ex. water" class="product-input" required>' +
+        "</div>" +
+        '<div class="input-box">' +
+        '<span class="details">Quantity</span>' +
+        `<input type="number" name="quantity-input-${counter}"` +
+        counter +
+        '" placeholder="Quantity" class="quantity-input" required>' +
+        "</div>";
+
+      $("#form-ann").append(newInputFields);
+
+      initializeAutocomplete($(".product-input").last());
+
+      //Move the buttons to the end of the form
+      $(".button").appendTo("#form-ann");
+      $("<br>").appendTo("#form-ann");
+      $("#addann").appendTo("#form-ann");
+
+      $("#hiddenInput").val(counter);
+    });
+  });
 });
