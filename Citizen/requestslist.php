c76a845c16c75dbc4b72e8e07871756554f5f2db
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-03-10 16:53:15 +0200

    Update requestslist.php

--- Citizen/requestslist.php
+++ Citizen/requestslist.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title> Requests List </title>
-        <link rel="stylesheet" href="requestslist.css?v=3">
+        <link rel="stylesheet" href="requestslist.css">
 </head>
 
 
@@ -20,13 +20,18 @@
     </div>
 </div>
 </body>
-
+<div class="form-box">
+            <table>
+                <thead>
+                    <tr>
 <?php
 include_once 'connect_db.php';
 
 // Fetch data from the database
 $sql = "SELECT req_product, demand, req_date, under_date, veh_username FROM requests";
 $result = $conn->query($sql);
+
+
 
 // Check if there are any results
 if ($result->num_rows > 0) {
@@ -62,3 +67,6 @@ if ($result->num_rows > 0) {
 // Close the database connection
 $conn->close();
 ?>
+</tr>
+</thead>
+<tbody>
