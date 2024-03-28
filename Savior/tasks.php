b211082d3a3b8b4670f806f1d387b5ff16d8cd3a
Nasia <101358901+nasia99@users.noreply.github.com>
2024-03-28 18:15:14 +0200

    Add files via upload

---
+++ Savior/tasks.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lan="en" and dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title> Tasks </title>
+        <link rel="stylesheet" href="tasks.css">
+</head>
+
+<body>
+<div class="main"> 
+    <div class="navbar">
+        <ul>
+            <li><a href= "homesavior.php">HOME</a></li>
+            <li><a href= "tasks.php">TASKS</a></li>
+            <li><a href="logout.php">LOGOUT</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
