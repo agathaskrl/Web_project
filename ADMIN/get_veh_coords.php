15c8b20f26bfe18f7f87509ecdd62771f4800bc0
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-04-16 12:36:09 +0300

    Add files via upload

---
+++ ADMIN/get_veh_coords.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'connect_db.php';
+
+$sql = 'SELECT lat, lng FROM coordinates INNER JOIN user ON user.username = coordinates.username WHERE user.role = "SAVIOR"';
+$result = $conn->query($sql);
+
+$coords_vehicles = array();
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $coords_vehicles[] = array("lat" => $row["lat"], "lng" => $row["lng"]);
+    }
+    echo json_encode($coords_vehicles);
+} else {
+    echo json_encode(array()); 
+}
+
+$conn->close();
+
