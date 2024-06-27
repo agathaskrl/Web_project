c43ce033382786d759d4266e424b536baf081e93
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-06-27 20:20:32 +0300

    Update updatewarehouse.php

--- ADMIN/updatewarehouse.php
+++ ADMIN/updatewarehouse.php
@@ -19,6 +19,41 @@
                 <li><a href="logout.php">LOG OUT</a></li>              
             </ul>
         </div>
+        <?php
+    include_once 'connect_db.php';
+session_start();
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    $query = "SELECT role FROM user WHERE username='$username'";
+    $result = mysqli_query($conn, $query);
+    
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['role'] = $row['role']; 
+
+    }
+}
+
+function checkLoggedIn() {
+      // Elegxos gia to an o xrhsths einai syndedemenos
+    if (!isset($_SESSION['username'])) {
+        echo '<div style="text-align: center; padding: 80px; color: rgba(76, 56, 30, 1); ">';
+        echo 'User not logged in!';
+        echo '</div>';
+        exit(); 
+    }
+        // Elegxos gia to an o rolos tou xrhsth einai  "SAVIOR" h "CITIZEN" kai aporich peraitero prosvashs
+    if (isset($_SESSION['role']) && ($_SESSION['role'] == "SAVIOR" || $_SESSION['role'] == "CITIZEN")) {
+        echo '<div style="text-align: center; padding: 80px; color: rgba(76, 56, 30, 1); ">';
+        echo 'Unauthorized access!';
+        echo '</div>';
+        exit(); 
+    }
+}
+
+checkLoggedIn();
+?>
         <br>
 
         <div class="container">
@@ -47,7 +82,7 @@
 <?php
 include_once 'connect_db.php';
 if (isset($_POST['truncate_elements'])) {
-    // Truncate the products table
+    // Adiasma tou pinaka procucst
     $truncateProducts = mysqli_query($conn, "TRUNCATE TABLE products");
     if ($truncateProducts) {
         echo "Products table truncated successfully.";
@@ -55,7 +90,7 @@ if (isset($_POST['truncate_elements'])) {
         echo "Error truncating products table: " . mysqli_error($conn);
     }
 
-    // Truncate the categories table
+    // Adiasma toy pinaka categories
     $truncateCategories = mysqli_query($conn, "TRUNCATE TABLE categories");
     if ($truncateCategories) {
         echo "Categories table truncated successfully.";
@@ -63,14 +98,15 @@ if (isset($_POST['truncate_elements'])) {
         echo "Error truncating categories table: " . mysqli_error($conn);
     }
 
+ 
     exit();
 }
 if (isset($_FILES['file'])) {
     
-    //json data from te file
+    //Dedomena json apo to arxeio 
     $jsonData = file_get_contents($_FILES['file']['tmp_name']);
 
-    //decode the json into an array 
+    //Apokodikopoihsh twn deodmenen se pinaka
     $data = json_decode($jsonData, true);
 
     if ($data === null) {
@@ -78,27 +114,30 @@ if (isset($_FILES['file'])) {
         exit();
     }
 
+    //Proetimasia twn quries
     $sql = "INSERT INTO products (id, name, category, detail_name, detail_value, quantity) VALUES (?, ?, ?, ?, ?, FLOOR(RAND()*(200-50+1))+50) 
         ON DUPLICATE KEY UPDATE name = VALUES(name), category = VALUES(category), detail_name = VALUES(detail_name), detail_value = VALUES(detail_value), quantity = FLOOR(RAND()*(200-50+1))+50;";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("issss", $id, $name, $category, $detail_name, $detail_value);
+//Gia kathe item 
 foreach ($data['items'] as $item) {
-    //check if any record exists in the database
+    //Elegxei ean yparxei sth vash ston pinaka products
     $existingRecord = mysqli_query($conn, "SELECT * FROM products WHERE id = '{$item['id']}'");
     if (mysqli_num_rows($existingRecord) > 0) {
         echo "Record with ID {$item['id']} already exists. Skipping insertion.<br>";
         continue;
     }
 
-    //extract the data from the array 
+    //Ejagwgh twn deodmenwn apo ton pinaka
     $id = $item['id'];
     $name = $item['name'];
     $category = $item['category'];
     foreach ($item['details'] as $detail) {
         $detail_name = isset($detail['detail_name']) ? $detail['detail_name'] : '';
         $detail_value = isset($detail['detail_value']) ? $detail['detail_value'] : '';
- 
+
+        //Eketelash twn queries
         if ($stmt->execute()) {
             echo "Item inserted successfully.<br>";
         } else {
@@ -110,8 +149,9 @@ if (isset($_FILES['file'])) {
     $sql2 = "INSERT INTO categories (id, category_name) VALUES (?, ?) ON DUPLICATE KEY UPDATE category_name = VALUES(category_name)";
     $stmt2 = $conn->prepare($sql2);
     $stmt2->bind_param("is", $id, $category_name);
-    //We choose categories bracause thats what its called in the json so we can extract the data 
+    //Epilegoume na ta onomasou categories giati etsi einai orismeno sto json arxeio 
     foreach ($data['categories'] as $category) {
+        //Exagwgh
         $id = $category['id'];
         $category_name = $category['category_name'];
 
@@ -132,6 +172,7 @@ if (isset($_FILES['file'])) {
 }
 
 
+// Kleisimo twn syndesewn
 $stmt->close();
 $stmt2->close();
 $conn->close();
