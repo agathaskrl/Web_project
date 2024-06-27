d3b39345b93120fa4515eb19c86926557619fe06
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-06-27 20:28:23 +0300

    Update get_citizen_coords.php

--- Citizen/get_citizen_coords.php
+++ Citizen/get_citizen_coords.php
@@ -2,28 +2,28 @@
 
 include_once 'connect_db.php';
 
-// Check if a user is logged in
+// Elegxos gia to an o xrhsths einai syndedemenos
 session_start();
 if (!isset($_SESSION['username'])) {
     echo "User is not logged in";
     exit(); // Stop further execution
 }
 
-// Get the current session's username
+// Pairnoyme to username toy syndedomenou xrhsth
 $username = $_SESSION['username'];
 
-// Get the coordinates for the current session's savior from the database
+// painroume ta coordiantes toy syndendmenou xrhsth
 $sql = "SELECT coordinates.lat, coordinates.lng 
         FROM coordinates 
         INNER JOIN user ON coordinates.username = user.username 
-        WHERE user.role = 'citizen' AND user.username = '$username'"; // Assuming 'username' is the session identifier
+        WHERE user.role = 'citizen' AND user.username = '$username'"; 
 
 $result = $conn->query($sql);
 
-$markerData = array(); // Initialize an array to hold the coordinates
+$markerData = array(); //Piankas gia ta coordinates
 
 if ($result->num_rows > 0) {
-    // Fetch each row and store it in the array
+    // Fernei kathe grammmh 
     while ($row = $result->fetch_assoc()) {
         $markerData[] = array("lat" => $row["lat"], "lng" => $row["lng"]);
     }
@@ -31,9 +31,9 @@ if ($result->num_rows > 0) {
     echo "No marker coordinates found for the current session's citizen";
 }
 
-// Encode the array as JSON and output it
+// Apokodikopoihsh ton pinaka san JSON 
 echo json_encode($markerData);
 
-// Close the database connection
+//Kleisimo ths syndeshs
 $conn->close(); 
 ?>
