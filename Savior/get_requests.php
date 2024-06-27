5903ccaa535fbb9f7466ea5b0c3790b8f920f2ac
Nasia <101358901+nasia99@users.noreply.github.com>
2024-06-27 14:47:43 +0300

    Update get_requests.php

--- Savior/get_requests.php
+++ Savior/get_requests.php
@@ -4,12 +4,12 @@ include_once 'connect_db.php';
 $sql = "SELECT req_id, civ_name, civ_surname, civ_phone, req_product, demand, lat, lng, veh_username,req_date, under_date, status FROM requests";
 $result = $conn->query($sql);
 
-// Check if there are results
+// Check an uparxoun apotelesmata
 if ($result->num_rows > 0) {
 
     $requests = array();
 
-    // Fetch and store each request
+    // Fetch kai apothikeusi kathe request
     while ($row = $result->fetch_assoc()) {
         $request = array(
             'req_id' => $row['req_id'],
@@ -25,18 +25,18 @@ if ($result->num_rows > 0) {
             'under_date' => $row['under_date'],
             'status' => $row['status'], 
         );
-        // Add the request to the requests array
+        // prosthiki tou request ston pinaka requests
         $requests[] = $request;
     }
 
-    // Convert requests array to JSON format
+    // metatropi tou pinaka requests se JSON morfi
     $jsonRequests = json_encode($requests);
 
-    // Output JSON data
+    // ektypwsi JSON data
     header('Content-Type: application/json');
     echo $jsonRequests;
 } else {
-    // No requests found
+    // minima No requests found
     echo json_encode(array('message' => 'No requests found'));
 }
 ?>
