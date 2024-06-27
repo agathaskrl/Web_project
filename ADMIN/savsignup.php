9a51b1e350baf33fd646e10d6b2e32198a935bce
Agathi Skyrla <101355370+agathaskrl@users.noreply.github.com>
2024-06-27 20:13:41 +0300

    Update savsignup.php

--- ADMIN/savsignup.php
+++ ADMIN/savsignup.php
@@ -35,14 +35,14 @@ if (isset($_SESSION['username'])) {
 }
 
 function checkLoggedIn() {
-    // Check if the user is not logged in
+     // Elegxos gia to an o xrhsths einai syndedemenos
     if (!isset($_SESSION['username'])) {
         echo '<div style="text-align: center; padding: 80px; color: rgba(76, 56, 30, 1); ">';
         echo 'User not logged in!';
         echo '</div>';
         exit(); 
     }
-    
+    // Elegxos gia to an o rolos tou xrhsth einai  "SAVIOR" h "CITIZEN" kai aporich peraitero prosvashs
     if (isset($_SESSION['role']) && ($_SESSION['role'] == "SAVIOR" || $_SESSION['role'] == "CITIZEN")) {
         echo '<div style="text-align: center; padding: 80px; color: rgba(76, 56, 30, 1); ">';
         echo 'Unauthorized access!';
@@ -106,6 +106,7 @@ checkLoggedIn();
 
 
 <?php
+//apo thn forma pou symplhrwse o xrhsths me to sumbit pernaei ta dedomena ston pinaka user
 if(isset($_POST['phone']))
 {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
